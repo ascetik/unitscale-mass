0a5c5630b91c68fde8c1d5ea445d995dbe441f65
ascetik <ascetik@free.fr>
2023-09-23 19:55:02 +0200

    doc and tests ok

--- src/Factories/MassScaler.php
+++ src/Factories/MassScaler.php
@@ -1,11 +1,28 @@
 <?php
 
+/**
+ * This is part of the UnitScale Mass package.
+ *
+ * @package    unitscale-mass
+ * @category   ScaleValue Factory
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+ declare(strict_types=1);
+
 namespace Ascetik\UnitscaleMass\Factories;
 
 use Ascetik\UnitscaleCore\Extensions\AdjustedValue;
 use Ascetik\UnitscaleCore\Types\ScaleValueFactory;
 use Ascetik\UnitscaleMass\Values\MassScaleValue;
 
+/**
+ * Build a MassScaleValue
+ *
+ * @version 1.0.0
+ */
 class MassScaler implements ScaleValueFactory
 {
     public static function unit(int|float $value, string $unit = ''): MassScaleValue
