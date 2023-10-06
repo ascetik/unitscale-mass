17b5da182cbfe6173e494e28c19ce14dc37fb7e0
ascetik <ascetik@free.fr>
2023-10-06 12:34:41 +0200

    MassScaler __callStatic method
    Code repeat from core Scaler. have to update core package...

--- src/Factories/MassScaler.php
+++ src/Factories/MassScaler.php
@@ -15,6 +15,7 @@
 namespace Ascetik\UnitscaleMass\Factories;
 
 use Ascetik\UnitscaleCore\Extensions\AdjustedValue;
+use Ascetik\UnitscaleCore\Parsers\ScaleCommandParser;
 use Ascetik\UnitscaleCore\Types\ScaleValueFactory;
 use Ascetik\UnitscaleMass\Values\MassScaleValue;
 
@@ -25,13 +26,30 @@ use Ascetik\UnitscaleMass\Values\MassScaleValue;
  */
 class MassScaler implements ScaleValueFactory
 {
-    public static function unit(int|float $value, string $unit = ''): MassScaleValue
+    public static function unit(int|float $value): MassScaleValue
     {
         return new MassScaleValue($value);
     }
 
-    public static function adjust(int|float $value, string $unit = ''): AdjustedValue
+
+    /**
+     * Use commands prefixed by "from"
+     *
+     * @param  string $method prefixed ScaleFactory method
+     * @param  mixed  $args   value and unit to use,
+     *
+     * @return void
+     */
+    public static function __callStatic(string $method, $args): MassScaleValue
     {
-        return self::unit($value)->adjust();
+        $checker = new ScaleCommandParser('from');
+        $command = $checker->parse($method)->name;
+        [$value, $unit] = match (count($args)) {
+            2 => [...$args],
+            1 => [$args[0], ''],
+            default => [0, '']
+        };
+        return MassScaleValue::createFromScale((float) $value, $command, $unit);
     }
+
 }
